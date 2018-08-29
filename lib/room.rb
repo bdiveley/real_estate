12261cc39274018372f0ca54ca6d10ea32db1461
Bailey Diveley <bailey.diveley@gmail.com>
2018-08-29 13:08:01 -0600

    Complete iteration 1

---
+++ lib/room.rb
@@ -0,0 +1,10 @@
+class Room
+  attr_accessor :category,
+                :area 
+
+  def initialize(category, length, width)
+    @category = category
+    @area = length * width
+  end
+
+end
---
+++ test/room_test.rb
@@ -0,0 +1,24 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/room'
+require 'pry'
+
+class RoomTest < Minitest::Test
+
+  def test_instance_exists
+    room = Room.new(:bedroom, 10, 13)
+    assert_instance_of Room, room
+  end
+
+  def test_new_category
+    room = Room.new(:bedroom, 10, 13)
+    assert_equal :bedroom, room.category
+  end
+
+  def test_area_calculates_correctly
+    room = Room.new(:bedroom, 10, 13)
+    assert_equal 130, room.area
+  end 
+
+
+end
