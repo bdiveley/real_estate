d0563304cd70a6c571dc122fd75da80571518bbd
Bailey Diveley <bailey.diveley@gmail.com>
2018-08-29 13:36:36 -0600

    complete iteration 3

--- lib/house.rb
+++ lib/house.rb
@@ -13,4 +13,16 @@ class House
     @rooms << room
   end
 
+  def rooms_from_category(room_type)
+    @rooms.find_all do |room|
+      room.category == room_type
+    end
+  end
+
+  def area
+    @rooms.reduce(0) do |sum, room|
+      sum + room.area
+    end
+  end
+
 end
